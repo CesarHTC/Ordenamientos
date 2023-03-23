b8888308e304172d1b80a90a20a1c31c35f97f94
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-03-22 19:35:59 -0600

    Quick-Sort

--- Quick-Sort.py
+++ Quick-Sort.py
@@ -3,16 +3,12 @@ import numpy
 numeros_aleatorios = [random.randint(1,50) for i in range(20)]
 print("La lista desordenada: ",numeros_aleatorios)
 def quick_sort(nums):
-
     if len(nums) <= 1:
         return nums
-        
     else:
         pivot = nums[0]
-
         numeros_menores = [num for num in nums[1:] if num <= pivot]#si el numero es menor que el pivote lo guarda
-        numeros_mayores = [num for num in nums[1:] if num > pivot]#si el numero es mayores que el pivote lo guarda
-        
+        numeros_mayores = [num for num in nums[1:] if num > pivot]#si el numero es menor que el pivote lo guarda
         return quick_sort(numeros_menores) + [pivot] + quick_sort(numeros_mayores)
 
 ordenados = quick_sort(numeros_aleatorios)
